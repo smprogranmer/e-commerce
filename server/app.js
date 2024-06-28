50e7ab38c9461c11886e6a0c8e97208ed6b1ec0d
S.m Programmer <s.mprogrammer&88@gmail.com>
2024-06-28 19:17:42 +0600

    change cors

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ config({
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173","http://localhost:4173",process.env.CLIENT_URL],
+  origin: ["http://localhost:5173","http://localhost:4173","https://e-commerce-4v4r.vercel.app/"],
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
 };
