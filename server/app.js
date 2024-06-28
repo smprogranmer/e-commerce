2edf33c8b2e2eb4261468bf6e8d9f2982df41932
S.m Programmer <s.mprogrammer&88@gmail.com>
2024-06-28 18:47:21 +0600

    fix cors error

--- server/app.js
+++ server/app.js
@@ -14,10 +14,11 @@ config({
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
+  origin: ["http://localhost:5173","http://localhost:4173",process.env.CLIENT_URL],
+  methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
-}
+};
+
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true,limit: "50mb" }));
 app.use(express.json({limit:"50mb"}));
